64b0eb46d99482ba2f7d5073e231388de79e12cc
StergiosFotoglou <stfotoglou@gmail.com>
2023-01-04 03:25:32 +0200

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'eb0758c1-5692-443a-8367-bfcb51551d23';
+// If the API key does not work try generating one here https://forkify-api.herokuapp.com/v2
+export const MODAL_CLOSE_SEC = 2.5;
